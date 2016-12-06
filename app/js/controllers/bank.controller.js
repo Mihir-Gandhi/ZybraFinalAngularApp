ce512607357a59131817bf108ff52eeed7e1521b
Mihir-Gandhi <mihirgandhi03@gmail.com>
2016-12-06 18:06:26 +0530

    Final Commit Without Service Code

---
+++ app/js/controllers/bank.controller.js
@@ -0,0 +1,49 @@
+﻿(function () {
+    'use strict';
+    angular
+        .module('ZybraBankApp')
+        .controller('BankController', ['BankService', BankController]);
+
+    function BankController(BankService) {
+        var self = this;
+        self.BankService = BankService;
+        self.selectedBankName = '';
+        self.getBankData();
+    }
+
+    BankController.prototype.getBankData = function () {
+        var self = this;
+        return self.BankService.getAllBanks()
+      .then(function (searchResult) {
+          if (searchResult)
+              self.bankData = searchResult.banks;
+      }, function (e) { self.handleError(e); });
+    }
+
+    BankController.prototype.getCityNames = function (selectedbankName) {
+        var self = this;
+        var Bank = { bankName: selectedbankName };
+        return self.BankService.getAllCityNames(Bank)
+      .then(function (searchResult) {
+          self.cityData = [];
+          for (var i = 0; i < searchResult.cities.length; i++) {
+              self.cityData.push({ cityName: searchResult.cities[i] });
+          }
+      }, function (e) { self.handleError(e); });
+    }
+
+    BankController.prototype.getBranchNames = function (selectedbankName, selectedCityName) {
+        var self = this;
+        var BranchRequest = { bank_name: selectedbankName, city: selectedCityName };
+        return self.BankService.getAllBranches(BranchRequest)
+      .then(function (searchResult) {
+          if (searchResult)
+              self.branchData = searchResult.branches;
+      }, function (e) { self.handleError(e); });
+    }
+
+    BankController.prototype.handleError = function (error) {
+        var self = this;
+        console.log(error);
+    }
+})();
---
+++ app/js/services/bank.services.js
@@ -0,0 +1,38 @@
+﻿(function () {
+    'use strict';
+    angular
+        .module('ZybraBankApp')
+        .service("BankService", ['$http', '$resource', '$q', BankService]);
+
+    function BankService($http, $resource, $q) {
+        this.getAllBanks = function () {
+            return $http.get('https://intense-everglades-72453.herokuapp.com/api/banks', { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } }).then(function (response) {
+                if (response) {
+                    return response.data;
+                }
+            });
+        };
+
+        this.getAllCityNames = function (data) {
+            var url = "https://intense-everglades-72453.herokuapp.com/api/city/" + data.bankName;
+            return $http.get(url, { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } }).then(function (response) {
+                if (response) {
+                    return response.data;
+                }
+            });
+        };
+
+        this.getAllBranches = function (branchRequest) {
+            return $http({
+                method: 'POST',
+                url: 'https://intense-everglades-72453.herokuapp.com/api/branches',
+                data: { bank_name: branchRequest.bank_name, city : branchRequest.city },
+                headers: { 'Content-Type': 'application/json' }
+            }).then(function (response) {
+                if (response) {
+                    return response.data;
+                }
+            });
+        };
+    }
+})();
